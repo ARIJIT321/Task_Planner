ac11f0651de8f9d9166712e27ad0a258ad4dedc1
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-03-24 22:43:34 +0530

    Adding Exception classes to handel error

---
+++ TaskPlanner/src/main/java/com/task/exception/SprintException.java
@@ -0,0 +1,13 @@
+package com.task.exception;
+
+public class SprintException extends Exception{
+
+	public SprintException() {
+		super();
+	}
+
+	public SprintException(String message) {
+		super(message);
+	}
+
+}
